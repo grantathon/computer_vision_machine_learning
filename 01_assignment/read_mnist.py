8a2bd42565755460012c0a0b0b6e12b56223c758
grantathon <grant.bartel@gmail.com>
2015-04-20 14:59:21 +0200

    Finished MNIST binary to image converter, added MNIST datasets

---
+++ 01_assignment/read_mnist.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import struct
+from array import array
+import numpy as np
+
+
+def read_mnist(path, dataset):
+    # Determine whether to pull training or testing data
+    if dataset == 'training':
+        filename_img = os.path.join(path, 'train-images.idx3-ubyte')
+        filename_lbl = os.path.join(path, 'train-labels.idx1-ubyte')
+    elif dataset == 'testing':
+        filename_img = os.path.join(path, 't10k-images.idx3-ubyte')
+        filename_lbl = os.path.join(path, 't10k-labels.idx1-ubyte')
+    else:
+        raise ValueError, "Dataset must be 'testing' or 'training'"
+
+    # Read and save labels
+    with open(filename_lbl, 'rb') as f:
+        magic_num, size = struct.unpack(">II", f.read(8))
+        label_raw = array('b', f.read())
+
+    # Read and save images
+    with open(filename_img, 'rb') as f:
+        magic_num, size, rows, cols = struct.unpack(">IIII", f.read(16))
+        image_raw = array('B', f.read())
+
+    N = size
+    labels = np.zeros(shape=(N, 1), dtype='int8')
+    images = np.zeros(shape=(N, rows * cols), dtype='uint8')
+    for i in range(N):
+        labels[i] = label_raw[i]
+        images[i] = np.array(image_raw[i*rows*cols : (i + 1)*rows*cols])
+
+    return labels, images
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "Please provide valid input parameters ([path to MNIST files] ['training' or 'testing' (optional)])"
+        exit(1)
+    path = sys.argv[1]
+    if len(sys.argv) == 3:
+        dataset = sys.argv[1]
+    else:
+        dataset = 'training'
+
+    # Convert MNIST binary to image and save to file
+    labels, images = read_mnist(path=path, dataset=dataset)
+    print labels
+    print images
