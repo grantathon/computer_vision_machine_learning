25725811225923a213f6da79d50521b740bda6fb
grantathon <grant.bartel@gmail.com>
2015-04-20 17:11:17 +0200

    Implemented grid search optimization

--- 01_assignment/svm_mnist.py
+++ 01_assignment/svm_mnist.py
@@ -1,16 +1,21 @@
 import sys
 import numpy as np
 from pprint import pprint
-from sklearn import svm
+from sklearn import svm, grid_search
 from read_mnist import *
 
 
 def train_mnist_svm(labels, images, num_samples, optimize=False):
-    print 'Training the SVM...'
-    class_fit = svm.SVC(kernel='poly', degree=2)
-    class_fit.fit(images[0:num_samples], labels[0:num_samples])
+    if optimize:
+        print 'Training the SVM w/optimization...'
+        clf = optimize_mnist_svm(opt_method='grid_search')
+    else:
+        print 'Training the SVM w/o optimization...'
+        clf = svm.SVC(kernel='poly', degree=2)
 
-    return class_fit
+    clf.fit(images[0:num_samples], labels[0:num_samples])
+
+    return clf
 
 def test_mnist_svm(labels, images, _svm):
     size = len(labels)
@@ -32,25 +37,44 @@ def test_mnist_svm(labels, images, _svm):
 
     return stats
 
-def optimize_mnist_svm(_svm):
-    pass
+def optimize_mnist_svm(opt_method):
+    if opt_method == 'grid_search':
+        # Default hyper-parameter grid
+        param_grid = [
+            {'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+            {'C': [1, 10, 100, 1000], 'gamma': [0.0, 0.001, 0.0001], 'kernel': ['rbf']},
+            {'C': [1, 10, 100, 1000], 'gamma': [0.0, 0.001, 0.0001], 'kernel': ['sigmoid']},
+            {'C': [1, 10, 100, 1000], 'gamma': [0.0, 0.001, 0.0001], 'degree': [2, 3], 'kernel': ['poly']},
+        ]
+
+        # Perform an exhaustive search over the hyper-parameter grid
+        clf = grid_search.GridSearchCV(estimator=svm.SVC(), param_grid=param_grid, scoring='accuracy')
+    else:
+        print "ERROR: Unexpected optimization method %s" % (opt_method)
+        exit(1)
+
+    return clf
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print "Please provide valid input parameters ([path to MNIST files] ['training' or 'testing'] [number of samples])"
+    if len(sys.argv) != 5:
+        print "Please provide valid input parameters ([path to MNIST files] ['training' or 'testing'] [number of samples] [optimize])"
         exit(1)
     path = sys.argv[1]
     dataset = sys.argv[2]
     num_samples = int(sys.argv[3])
+    optimize = bool(int(sys.argv[4]))
 
     # Load training and testing data
     train_labels, train_images = read_mnist(path, 'training')
     test_labels, test_images = read_mnist(path, 'testing')
 
     # Build the MNIST SVM
-    _svm = train_mnist_svm(labels=train_labels, images=train_images, num_samples=num_samples, optimize=False)
-    print '\nHyper Parameters:'
+    _svm = train_mnist_svm(labels=train_labels, images=train_images, num_samples=num_samples, optimize=optimize)
+    print '\nHyper-Parameters:'
     pprint(_svm.get_params())
+    if optimize:
+        print '\nOptimal Hyper-Parameters:'
+        pprint(_svm.best_params_)
     print
 
     # Test the MNIST SVM
