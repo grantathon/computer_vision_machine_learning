73e9591c344fe78f9726c545598c56b928610125
grantathon <grant.bartel@gmail.com>
2015-05-03 22:32:57 +0200

    Completed assignment 2 code

--- 02_assignment/classify_imagenet_script.py
+++ 02_assignment/classify_imagenet_script.py
@@ -2,6 +2,7 @@ import os
 from os.path import expanduser
 import sys
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
@@ -12,27 +13,28 @@ if __name__ == "__main__":
 	img_uri = sys.argv[1]
 
 	# Make sure that caffe is on the python path:
-	caffe_root = expanduser("~") + '/caffe/'  # this file is expected to be in {caffe_root}/examples
-	sys.path.insert(0, caffe_root + 'python')
+	home_dir = expanduser("~")
+	caffe_root = home_dir + '/caffe'  # this file is expected to be in {caffe_root}/examples
+
+	sys.path.insert(0, caffe_root + '/python')
 
 	import caffe
 
 	# Set the right path to your model definition file, pretrained model weights,
 	# and the image you would like to classify.
-	MODEL_FILE = '/usr/prakt/p045/caffe/models/bvlc_reference_caffenet/deploy.prototxt'
-	PRETRAINED = '/usr/prakt/p045/caffe/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+	MODEL_FILE = caffe_root + '/models/bvlc_reference_caffenet/deploy.prototxt'
+	PRETRAINED = caffe_root + '/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
 
-	# import os
 	if not os.path.isfile(PRETRAINED):
 	    print("Downloading pre-trained CaffeNet model...")
-	    sys.argv.append(caffe_root + 'models/bvlc_reference_caffenet')
-	    execfile(caffe_root + 'scripts/download_model_binary.py')
+	    sys.argv.append(caffe_root + '/models/bvlc_reference_caffenet')
+	    execfile(caffe_root + '/scripts/download_model_binary.py')
 
 	# Setup neural network classifier
 	# caffe.set_mode_cpu()
 	caffe.set_mode_gpu()
 	net = caffe.Classifier(MODEL_FILE, PRETRAINED,
-	                       mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
+	                       mean=np.load(caffe_root + '/python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
 	                       channel_swap=(2,1,0),
 	                       raw_scale=255,
 	                       image_dims=(256, 256))
@@ -45,12 +47,44 @@ if __name__ == "__main__":
 
 	# Predict the classification of the image and display stats
 	prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
-	print 'predicted class:', prediction[0].argmax()
-	
+
 	# Display class prediction histogram
 	plt.subplot(2, 1, 2)
 	plt.plot(prediction[0])
 	plt.ylabel('Probability')
 	plt.xlabel('Class Index')
+
+	# plt.show()
+
+	# if not os.path.isfile(CLASS_IDX_MAP):
+	#     print("Downloading ImageNet class index to name map...")
+	#     del sys.argv[:]
+	#     print sys.argv
+	#     sys.argv.append(caffe_root + '/data/ilsvrc12/')
+	#     print sys.argv
+	#     execfile(caffe_root + '/data/ilsvrc12/get_ilsvrc_aux.sh')
+	
+	# Retrieve the name and ID for each class
+	with open(caffe_root + '/data/ilsvrc12/synset_words.txt') as f:
+	    labels_df = pd.DataFrame([
+	        {
+	            'synset_id': l.strip().split(' ')[0],
+	            'name': ' '.join(l.strip().split(' ')[1:]).split(',')[0]
+	        }
+	        for l in f.readlines()])
+
+	# Join predictions with proper names and IDs
+	labels_df.sort('synset_id')
+	predictions_df = pd.DataFrame(data=prediction[0], columns=['probability'])
+	predictions_df = predictions_df.join(labels_df)
+
+	# Find the top prediction and display stats
+	prediction = predictions_df.iloc[prediction[0].argmax()]
+	prediction_entropy = -np.array([e*np.log(e) for e in predictions_df['probability']]).sum()
+	print '\nPrediction:'
+	print '    Class name:    %s' % (prediction['name'])
+	print '    Synset ID:     %s' % (prediction['synset_id'])
+	print '    Probability:   %f' % (prediction['probability'])
+	print '    Entropy:       %f' % (prediction_entropy)
 
 	plt.show()
